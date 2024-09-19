5e0b0a153a9bc7e0fffb3503608da00b75ba19c7
abidt20ahmed <abidahmedmahin21@gmail.com>
2024-09-19 14:24:22 +0600

    view all button added

--- src/components/shared/project/ProjectSection.jsx
+++ src/components/shared/project/ProjectSection.jsx
@@ -1,16 +1,25 @@
 "use client"
 import React from 'react';
 import { Project } from './Project';
+import FadeUp from '../animation/FadeUp';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const ProjectSection = () => {
-    
+
     return (
-        <div className="relative my-10 md:my-20 p-3">
+        <div className="relative p-3 my-10 md:my-20">
             <h2 className="my-8 text-center section-heading">
-                    Our Work
-                </h2>
-            <Project/>
-            
+                Our Work
+            </h2>
+            <Project />
+
+            <FadeUp>
+                <Link href="/project-album" className='flex justify-center pt-10'>
+                    <Button >View all</Button>
+                </Link>
+            </FadeUp>
+
         </div>
     );
 };
