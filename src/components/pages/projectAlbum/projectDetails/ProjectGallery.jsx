24f53b36b1a7b3ece3bfb68182240bd2a99aa2a8
abidt20ahmed <abidahmedmahin21@gmail.com>
2024-09-20 09:02:34 +0600

    frame

--- src/components/pages/projectAlbum/projectDetails/ProjectGallery.jsx
+++ src/components/pages/projectAlbum/projectDetails/ProjectGallery.jsx
@@ -19,7 +19,7 @@ const ProjectGallery = ({ data = [], id }) => {
               <Link href={`/project-album/full-screen/${id}`} className="p-1  h-[400px] overflow-x-hidden overflow-y-auto relative group block">
                 <Image src={item} alt="project img" className="overflow-hidden " />
                 <div className="absolute inset-0 flex flex-col items-center justify-center transition-opacity duration-300 ease-in-out rounded-lg opacity-0 bg-slate-700/80 bg-opacity-70 group-hover:opacity-100">
-                  <SlFrame className="text-white transition-all duration-500 size-14 hover:size-16" />
+                  <SlFrame className="text-white size-14" />
                   <ProjectButton text="Full Screen" />
                 </div>
               </Link>
