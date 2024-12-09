e4e83cb40088333db01cee772578b0ec53584070
abidt20ahmed <abidahmedmahin21@gmail.com>
2024-12-09 13:08:17 +0600

    black friday offer

--- src/components/shared/advertisement/Popup.jsx
+++ src/components/shared/advertisement/Popup.jsx
@@ -16,9 +16,16 @@ export default function Popup() {
 
   const [isVisible, setisVisible] = useState(false);
 
-  setTimeout(() => {
-    setisVisible(true)
-  }, 5000);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setisVisible(true)
+
+    }, 5000);
+
+    return () => {
+    };
+  }, []);
 
   useEffect(() => {
     if (isVisible) {
@@ -49,49 +56,11 @@ export default function Popup() {
         >
           <MdClose className="w-6 h-6 p-1 bg-red-500 rounded-full" />
         </button>
-
         <Image src={BlackFriday} alt='Black friday Offer' />
-        {/* <div>
-          <Image className="w-60" src={shapeWave} alt="shapeWave" />
-        </div>
 
-        <Image
-          className="absolute w-10 right-10 top-16 animate-bounce"
-          src={dotsRight}
-          alt="shapeWave1"
-        />
-        <div className="absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
-          <p className="text-sm text-center">
-            Clooud Gen&apos;s
-          </p>
-          <h4 className="mb-2 text-center text-foreground">
-            Entrepreneurs
-          </h4>
-          <h3 className="mb-2 text-4xl text-foreground">CAMPAINGS</h3>
-          <h4 className="mb-2 text-2xl text-center">Ongoing</h4>
-          <p className="mb-2 text-center text-foreground">GET UP TO <span className='font-semibold '>35%</span> OFF</p>
-
-          <Link className="flex justify-center" href="/special-offer">
-            <Button variant="outline" className="font-bold border-[3px] border-foreground">
-              See Details
-            </Button>
-
-          </Link>
-        </div>
-
-        <Image
-          className="absolute w-10 bottom-16 left-10"
-          src={dotsLeft}
-          alt="shapeWave1"
-        />
-        <div className="absolute bottom-0 right-0 pointer-events-none">
-          <Image
-            className=" w-60"
-            src={shapeWave1}
-            alt="shapeWave"
-          />
-        </div> */}
       </div>
+
+
       {/* <div onClick={(e) => e.stopPropagation()} className="bg-white w-[450px] h-[450px] rounded shadow-lg relative overflow-hidden m-3">
         <div>
           <Image className="w-60" src={shapeWave} alt="shapeWave" />
